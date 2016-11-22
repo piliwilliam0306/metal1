5193295a09ba9df17ecbd2e451f521e7ae3ce29f
ChingHengWang <qoogood1234@gmail.com>
2016-11-23 03:58:19 +1030

    add shutdown service

--- andbot/src/startup_service.py
+++ andbot/src/startup_service.py
@@ -2,7 +2,7 @@
 
 #from beginner_tutorials.srv import *
 import os
-from openface4ARM.srv import *
+from andbot.srv import *
 import rospy
  
 def teleop(req):
@@ -16,12 +16,22 @@ def mapping(req):
 def navigation(req):
     os.system('roslaunch andbot 578.launch')
     return 'Killed'
+
+def reboot(req):
+    os.popen("sudo -S reboot", 'w').write('odroid\n')
+    return 'Killed'
+
+def shutdown(req):
+    os.popen("sudo -S shutdown -h now", 'w').write('odroid\n')
+    return 'Killed'
  
 def startup_server():
     rospy.init_node('startup_service')
     s1 = rospy.Service('teleop_service', Startup, teleop)
     s2 = rospy.Service('mapping_service', Startup, mapping)
     s3 = rospy.Service('navigation_service', Startup, navigation)
+    s4 = rospy.Service('reboot_service', Startup, reboot)
+    s5 = rospy.Service('shutdown_service', Startup, shutdown)
     print "Waiting for services to be called."
     rospy.spin()
    
